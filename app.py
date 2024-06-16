17a2b5873c426923abdafaef2fe8779fbe6cfa5d
Aliyah Sausan Huwel <ahuwel@Aliyahs-Air.lan>
2024-06-15 21:49:25 -0400

    Update to bind to Heroku port

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
 import os
-from flask import Flask, request, jsonify
-import pytesseract
-from PIL import Image
-import io
 
-app = Flask(__name__)
+tessdata_dir = "/usr/share/tesseract-ocr/4.00/tessdata/"
 
-# Set the TESSDATA_PREFIX environment variable
-os.environ["TESSDATA_PREFIX"] = "/usr/share/tesseract-ocr/4.00/tessdata/"
-
-@app.route('/ocr', methods=['POST'])
-def ocr():
-    print("Hello")
-    if 'image' not in request.files:
-        return jsonify({"error": "No image provided"}), 400
-
-    image_file = request.files['image']
-    image = Image.open(io.BytesIO(image_file.read()))
-    extracted_text = pytesseract.image_to_string(image, lang='eng')
-    return jsonify({"text": extracted_text})
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if os.path.exists(tessdata_dir):
+    print(f"Directory exists: {tessdata_dir}")
+    print("Contents:")
+    for filename in os.listdir(tessdata_dir):
+        print(filename)
+else:
+    print(f"Directory does not exist: {tessdata_dir}")
