777c845f14a32590b8319bd3a0626718e8d22065
Aliyah Sausan Huwel <ahuwel@Aliyahs-Air.lan>
2024-06-15 21:36:43 -0400

    Update to bind to Heroku port

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import io
 
 app = Flask(__name__)
 
+# Set the TESSDATA_PREFIX environment variable
+os.environ["TESSDATA_PREFIX"] = "/usr/share/tesseract-ocr/4.00/"
+
 @app.route('/ocr', methods=['POST'])
 def ocr():
     if 'image' not in request.files:
@@ -13,7 +16,7 @@ def ocr():
 
     image_file = request.files['image']
     image = Image.open(io.BytesIO(image_file.read()))
-    extracted_text = pytesseract.image_to_string(image)
+    extracted_text = pytesseract.image_to_string(image, lang='eng')
     return jsonify({"text": extracted_text})
 
 if __name__ == '__main__':
